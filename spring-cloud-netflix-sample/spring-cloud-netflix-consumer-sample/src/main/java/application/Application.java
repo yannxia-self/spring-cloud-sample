9d778181a065cc2ee12002c239f6046f96717812
yann <me.yan.xia@qq.com>
2017-03-03 17:56:12 +0800

    Add spring netflix

---
+++ spring-cloud-netflix-sample/spring-cloud-netflix-consumer-sample/src/main/java/application/Application.java
@@ -0,0 +1,25 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableEurekaClient
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
